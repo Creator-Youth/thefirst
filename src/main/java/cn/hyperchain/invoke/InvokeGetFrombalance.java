2f8d4afe5c5e633b4d65b02d53e11b34b2adef47
wangxianshou <wangxianshou@hyperchain.cn>
2019-12-03 10:48:09 +0800

    test in  dev 2

--- src/main/java/cn/hyperchain/invoke/InvokeGetFrombalance.java
+++ src/main/java/cn/hyperchain/invoke/InvokeGetFrombalance.java
@@ -26,6 +26,7 @@ public class InvokeGetFrombalance implements BaseInvoke<Integer,ISBank> {
     public static void test() {
         System.out.println("输出数据进行测试。第si次测试");
         System.out.println("输出数据进行测试。第si次测试,在dev上做了测试。");
+        System.out.println("输出数据进行测试。第si次测试,在dev上做了测试,并未提交。");
     }
 
 
